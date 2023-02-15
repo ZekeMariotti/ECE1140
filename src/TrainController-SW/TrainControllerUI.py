7a81c83c14271fe1b28c5a55926970df33091a82
ZekeMariotti <97479101+ZekeMariotti@users.noreply.github.com>
2023-02-15 01:01:51 -0500

    positions of elements updated, dynamic resizing partially setup

--- src/TrainController-SW/TrainControllerUI.py
+++ src/TrainController-SW/TrainControllerUI.py
@@ -3,49 +3,83 @@
 from distutils.cmd import Command
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QSlider, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import *
+from PyQt6 import QtCore
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtGui import *
 
 # Class for the main window
 class MainWindow(QMainWindow):
 
         # Constructor 
         def __init__(self):
-                super().__init__()
+            super().__init__()
+            self.buttonWidth = 175
+            self.buttonHeight = 50
+            self.labelWidth = self.buttonWidth*2
+            self.labelHeight = round(self.buttonHeight*1.3)
+            #self.emergencyBrakeEnable = None 
 
-                self.setWindowTitle("Train Controller")
-                self.resize(QSize(1200, 900))
-                self.setMinimumSize(QSize(600, 450))
-
-               # layout = QVBoxLayout()                
+            self.setWindowTitle("Train Controller")
+            self.setFixedSize(QSize(1366, 768))
+            #self.setFixedSize(QSize(1920, 1080))
                 
-                emergencyBrakeEnable = self.emergencyBrakeEnableSetup()
-                commandedSpeedSlider = self.commandedSpeedSliderSetup()                
-
+            self.emergencyBrakeState = self.emergencyBrakeStateSetup()
+            self.emergencyBrakeEnable = self.emergencyBrakeEnableSetup()
+            self.emergencyBrakeDisable = self.emergencyBrakeDisableSetup()
+            self.commandedSpeedSlider = self.commandedSpeedSliderSetup()       
+                
         # widget setups
+        def emergencyBrakeStateSetup(self):
+            emergencyBrakeState = QLabel()
+            emergencyBrakeState.setText("Emergency Brake: {State}")
+            emergencyBrakeState.setFixedSize(QSize(self.labelWidth, self.labelHeight))
+            emergencyBrakeState.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            emergencyBrakeState.move(round(self.frameGeometry().width()*0.05), round(self.frameGeometry().height()*0.25-emergencyBrakeState.frameGeometry().height()*0.5))
+            emergencyBrakeState.setParent(self)
+            return emergencyBrakeState
+
         def emergencyBrakeEnableSetup(self):
             emergencyBrakeEnable = QPushButton("Enable Emergency Brake")
-            emergencyBrakeEnable.setFixedSize(QSize(175, 50))
-            emergencyBrakeEnable.move(500, 500)
+            emergencyBrakeEnable.setFixedSize(QSize(self.buttonWidth, self.buttonHeight))
             emergencyBrakeEnable.clicked.connect(self.emergencyBrakeEnableClick)
-            emergencyBrakeEnable.move(500, 300)
+            emergencyBrakeEnable.move(round(self.frameGeometry().width()*0.05), round(self.frameGeometry().height()*0.3-emergencyBrakeEnable.frameGeometry().height()*0.5))
             emergencyBrakeEnable.setParent(self)
             return emergencyBrakeEnable
 
+        def emergencyBrakeDisableSetup(self):
+            emergencyBrakeDisable = QPushButton("Disable Emergency Brake")
+            emergencyBrakeDisable.setFixedSize(QSize(self.buttonWidth, self.buttonHeight))
+            emergencyBrakeDisable.clicked.connect(self.emergencyBrakeDisableClick)
+            emergencyBrakeDisable.move(round(self.frameGeometry().width()*0.05+emergencyBrakeDisable.frameGeometry().width()), round(self.frameGeometry().height()*0.3-emergencyBrakeDisable.frameGeometry().height()*0.5))
+            emergencyBrakeDisable.setParent(self)
+            return emergencyBrakeDisable
+
         def commandedSpeedSliderSetup(self):
             commandedSpeedSlider = QSlider(Qt.Orientation.Horizontal)
-            commandedSpeedSlider.setFixedSize(500, 40)
+            commandedSpeedSlider.setFixedSize(300, 40)
             commandedSpeedSlider.setRange(0, 100)
             commandedSpeedSlider.setSingleStep(1)
-            commandedSpeedSlider.move(500, 400)
+            commandedSpeedSlider.move(round(self.frameGeometry().width()*0.5-commandedSpeedSlider.frameGeometry().width()*0.5), round(self.frameGeometry().height()*0.5))
             commandedSpeedSlider.setParent(self)
             return commandedSpeedSlider
 
 
         # event actions
+
+        # might use for resizing elements
+        #def resizeEvent(self, event):
+        #    print("window resized")
+        #    emergencyBrakeEnable = self.emergencyBrakeEnable
+        #    emergencyBrakeEnable.move(round(self.frameGeometry().width()/10-emergencyBrakeEnable.frameGeometry().width()/2), round(self.frameGeometry().height()/3-emergencyBrakeEnable.frameGeometry().height()/2))
+        #    QMainWindow.resizeEvent(self, event)
+           
+
         def emergencyBrakeEnableClick(self):
-            print("Button Clicked")
+            print("Enable Clicked")
+
+        def emergencyBrakeDisableClick(self):
+            print("Disable Clicked")
 
 # class to create color widgets
 class Color(QWidget):
