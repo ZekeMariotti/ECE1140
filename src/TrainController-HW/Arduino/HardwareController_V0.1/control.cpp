9454856caee7e68d739ae59c1616fd2103e74efd
Raheel Farouk <raheelfarouk@gmail.com>
2023-02-21 16:54:23 -0500

    Add files

---
+++ src/TrainController-HW/Arduino/HardwareController_V0.1/control.cpp
@@ -0,0 +1,13 @@
+void calculatePower(int currentTime, int previousTime, int commandedSpeed,
+    int currentSpeed, float Ki, float Kp){
+      
+}
+
+void automaticSpeedControl(){
+
+}
+
+void calculateBrake(){
+
+}
+
